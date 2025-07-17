203a01fa60f7d54504f1422247560c1eadd49828
udehlee <ritzeepita@gmail.com>
2025-07-17 18:33:43 +0100

    setup user model and logger

---
+++ src/controller/authController.js
@@ -0,0 +1,11 @@
+import logger from "../utils/logger.js"
+
+
+const signup = async (res,req) => {
+
+}
+
+const login = async (res,req) => {
+
+
+}
---
+++ src/models/userModel.js
@@ -0,0 +1,13 @@
+ function User({ id, fullname, email, password, meterNumber, unitBalance }) {
+  return {
+    id,
+    fullname,
+    email,
+    password,
+    meterNumber,
+    unitBalance
+  };
+}
+
+
+export default { User }
