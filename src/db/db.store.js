90c2ed389b47346dcf952674206505d2c8d71d91
udehlee <ritzeepita@gmail.com>
2025-07-19 18:40:51 +0100

    add createUser and checkEmail functions for handling user database signup and login

---
+++ src/db/db.store.js
@@ -0,0 +1,41 @@
+import { user } from "../models/userModel.js";
+import { logger } from "../utils/logger.js";
+import { client } from "./db.conn.js";
+
+const createUser = async (data) => {
+  const { fullname, email, password, meterNumber, unitBalance = 0 } = data;
+  const query = ` 
+     INSERT INTO users (fullname, email, password, meter_number, unit_balance)
+     VALUES ($1, $2, $3, $4, $5)
+     RETURNING id,fullname,email,meter_number,unit_balance);`;
+
+  const result = await client.query(query, [
+    fullname,
+    email,
+    password,
+    meterNumber,
+    unitBalance,
+  ]);
+
+  return user(result.rows[0]);
+};
+
+const checkEmail = async () => {
+  try {
+    const query = `SELECT * FROM users WHERE email = $1`;
+    const result = await client.query(query, [email]);
+
+    if (result.rows.length === 0) {
+      logger.info("No user found with email: " + email);
+      return null;
+    }
+
+    const user = user(result.rows[0]);
+    return user;
+  } catch (err) {
+    logger.error("Error fetching user by email");
+    throw err;
+  }
+};
+
+export default { createUser, checkEmail };
