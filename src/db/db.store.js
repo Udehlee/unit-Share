a325840eb908d3aeebda7d763bf2ea380d37e8fd
udehlee <ritzeepita@gmail.com>
2025-07-19 18:41:44 +0100

    add createUser and checkEmail functions for handling user database signup and login

--- src/db/db.store.js
+++ src/db/db.store.js
@@ -3,7 +3,7 @@ import { logger } from "../utils/logger.js";
 import { client } from "./db.conn.js";
 
 const createUser = async (data) => {
-  const { fullname, email, password, meterNumber, unitBalance = 0 } = data;
+  const { fullname, email, password, meterNumber, unitBalance } = data;
   const query = ` 
      INSERT INTO users (fullname, email, password, meter_number, unit_balance)
      VALUES ($1, $2, $3, $4, $5)
