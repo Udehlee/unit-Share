9fad31d7375ffb9bd9f6ad57397d038de683363d
udehlee <ritzeepita@gmail.com>
2025-07-22 22:38:23 +0100

    update user model to include meter type and creation date

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,14 @@
- function User({ id, fullname, email, password, meterNumber, unitBalance }) {
+function user( {id, fullname, email, pass_word, meter_number, meter_type, unit_balance, created_at }) {
   return {
     id,
     fullname,
     email,
-    password,
-    meterNumber,
-    unitBalance
+    password: pass_word,
+    meterNumber: meter_number,
+    meterType: meter_type,
+    unitBalance: unit_balance,
+    createdAt: created_at,
   };
 }
 
-
-export default { User }
+export default { user };
