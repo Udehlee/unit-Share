a0f5d19ed64254f4803b0a4657dbf5d0aab2b17c
udehlee <ritzeepita@gmail.com>
2025-07-22 22:23:35 +0100

    add transaction route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup, login } from "../controller/authController.js"
+import { signup, login, transaction } from "../controller/authController.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/signup", signup)
-router.post("/login", login)
+router.post("/api/signup", signup);
+router.post("/api/login", login);
+router.post("/api/transaction", transaction);
 
-export default router
+export default router;
