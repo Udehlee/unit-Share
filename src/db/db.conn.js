ee8aa1223de1efc8ac9d040b9c3d6a1bbd64efc2
udehlee <ritzeepita@gmail.com>
2025-07-23 22:24:17 +0100

    change to  connection pool to handle many db connections

--- src/db/db.conn.js
+++ src/db/db.conn.js
@@ -2,12 +2,12 @@ import pg from "pg";
 import dotenv from "dotenv";
 import logger from "../utils/logger.js";
 
-const { Client } = pg;
+const { Pool } = pg;
 
 const loadEnv = dotenv.config();
 if (loadEnv.error) throw new Error("failed to load env. file");
 
-const client = new Client({
+const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
@@ -17,12 +17,13 @@ const client = new Client({
 
 const connectDB = async () => {
   try {
-    await client.connect();
-    logger.info("connected successfully to postgres");
+    const client = await pool.connect();
+    logger.info("Connected successfully to PostgreSQL");
+    client.release(); 
   } catch (err) {
-    logger.error("failed to connect to postgres", err);
+    logger.error("Failed to connect to PostgreSQL", err);
     process.exit(1);
   }
 };
 
-export default { client, connectDB };
+export { pool, connectDB };
