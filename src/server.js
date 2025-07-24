5bb269b1fb23d8b899c21880a565dbca124a138e
udehlee <ritzeepita@gmail.com>
2025-07-24 20:37:39 +0100

    setup server with db connection and routes

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import logger  from "./utils/logger.js";
+import { connectDB } from "./db/db.conn.js";
+import  routes  from "./routes/routes.js"; 
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/api/v1", routes);
+
+const startServer = async () => {
+  try {
+    await connectDB(); 
+    app.listen(process.env.PORT, () => {
+      logger.info("Server running at http://localhost:" + process.env.PORT);
+    });
+  } catch (err) {
+    logger.error("Failed to start server:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+
+
