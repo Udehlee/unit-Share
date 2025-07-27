1148eb445796deb1b97e4b1191466c98d7e92d08
udehlee <ritzeepita@gmail.com>
2025-07-27 20:00:43 +0100

    add transaction to handle transaction record creation request

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,25 +1,25 @@
-import { logger } from "../utils/logger.js";
-import { store } from "../db/db.store.js";
-import argon2 from 'argon2';
+import logger from "../utils/logger.js";
+import { saveUser, checkEmail, saveTransaction  } from "../db/db.store.js";
+import argon2 from 'argon2'
 
 //signup creates new user account
 const signup = async (req, res) => {
-  const { fullname, email, password, meterNumber, unitBalance } = req.body;
+  const { fullname, email, password, meterNumber, meterType, unitBalance } = req.body;
 
-  if (!fullname || !email || !password || !meterNumber || unitBalance === undefined) {
+  if (!fullname || !email || !password || !meterNumber|| !meterType || unitBalance === undefined) {
     logger.error("Missing user fields");
     return res.status(422).json({ success: false, message: "All fields are required" });
   }
 
   try {
-    const existingUser = await user.checkEmail(email);
+    const existingUser = await checkEmail(email);
     if (existingUser) {
       logger.info("User already exists with email: " + email);
       return res.status(409).json({ success: false, message: "User already exists" });
     }
 
     const hashedPasswd = await argon2.hash(password);
-    const newUser = await store.createUser({
+    const newUser = await saveUser({
       fullname,
       email,
       password: hashedPasswd, 
@@ -30,7 +30,7 @@ const signup = async (req, res) => {
     logger.info("User created successfully: " + email);
     return res.status(201).json({ success: true, message: "User created successfully", data: newUser });
   } catch (err) {
-    logger.error("error Signin in: " + err);
+    logger.error("failed to signin user" + err);
     return res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
@@ -44,7 +44,7 @@ const login = async (req, res) => {
   }
 
   try {
-    const existingUser = await store.checkEmail(email);
+    const existingUser = await checkEmail(email);
     if (!existingUser) {
       return res.status(401).json({ success: false, message: "user details are invalid" });
     }
@@ -65,9 +65,35 @@ const login = async (req, res) => {
 
     return res.status(200).json(loggedInUser);
   } catch (err) {
-    logger.error("error Login in: " + err);
+    logger.error("failed to Login in user: " + err);
     return res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
-export default { signup, login}
+//transaction creates transaction record upon sucessful transfer
+const transaction = async (req, res) => {
+  const { senderId, recipientId, unitsTransferred } = req.body
+
+  if (!senderId || !recipientId || !unitsTransferred ) {
+    return res.status(400).json({ success: false, message: "senderId, recipientId and unitsTransferred are required" });
+  }
+
+try {
+    const trxn = await saveTransaction({
+      senderId,
+      recipientId,
+      unitsTransferred,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Transaction completed successfully",
+      data: trxn,
+    });
+  } catch (err) {
+    logger.error("Failed to complete transaction" + err);
+    return res.status(500).json({success: false, message: "Internal server error"});
+  }
+};
+
+export { signup, login, transaction }
